f29021f9c6922a83440a3f6b74a1da8b5fe8128b
maycon <maycon@maycon-Q470C-500P4C>
2013-04-03 22:36:53 -0300

    Criação da classe que irá iniciar as operações.

---
+++ src/main/java/br/com/cnmminer/main/Principal.java
@@ -0,0 +1,26 @@
+package br.com.cnmminer.main;
+
+import java.awt.EventQueue;
+
+import br.com.cnmminer.formularios.FormBemVindo;
+import br.com.cnmminer.formularios.FormPrincipal;
+
+public class Principal {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FormPrincipal frame = new FormBemVindo();
+					frame.setFrameAtual(frame);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
